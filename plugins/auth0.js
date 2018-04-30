9f0f8438469e3998d36d88548263122df814daf3
kindaichidai <kindai.0911@gmail.com>
2018-04-30 18:22:44 +0900

    custom login page

--- plugins/auth0.js
+++ plugins/auth0.js
@@ -1,4 +1,6 @@
 import Auth0Lock from 'auth0-lock'
+import jwtDecode from 'jwt-decode'
+import queryString from 'query-string'
 
 const config = {
   cliendID: 'YCauVJl5fPY1Cp3XUDGWxWSN2TOMQEdD',
@@ -28,6 +30,27 @@ class Auth0Util {
   
   getBaseUrl() {
     return `${window.location.protocol}//${window.location.host}`
+  }
+  
+  getQueryParams() {
+    return queryString.parse(location.hash)
+  }
+  
+  setToken({access_token, id_token, expires_in}) {
+    const localStorage = window.localStorage
+    localStorage.setItem('accessToken', access_token)
+    localStorage.setItem('idToken', id_token)
+    localStorage.setItem('expiresAt', expires_in * 1000 + new Date().getTime())
+    localStorage.setItem('user', JSON.stringify(jwtDecode(id_token)))
+  }
+  
+  setTokenByQuery() {
+    this.setToken(this.getQueryParams());
+  }
+  
+  isAuthenticated() {
+    const expiresAt = window.localStorage.getItem('expiresAt')
+    return new Date().getItem < expiresAt
   }
 }
 
